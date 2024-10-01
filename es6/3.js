49162a2b5adb33dc1bb6a62385e807301cd86d4f
Ruchikakumbhare <ruchikakhumbhare@gmail.com>
2024-10-01 23:20:50 +0530

    Added

--- es6/3.js
+++ es6/3.js
@@ -1,91 +1,84 @@
-// function add(...arr){
-// console.log(arr)
+// short circuit operator 
+// falsy values  - NaN , null , undefined , 0 , 4  < 2 
+// truthy values -  1 , '1' , "c" ,true , 6 > 3
+//-------------------------program1
+//&& -->1 st false mila to short circuit
+// let val = 3>2 && 'ruchi'    //1 st true hai isly 2 nd check kiya or output mila
+// console.log(val)      //ruchi
+
+// let val1 = 3>10 && 'ruchi'      //false mila short circuit hogya
+// console.log(val1)     //false
+
+// let val2 = 0 && 'ruchi'
+// console.log(val2)      //0
+
+// let val3 = null && 'ruchi'
+// console.log(val3)     //null
+
+// let val4 = undefined && 'ruchi'
+// console.log(val4) 
+ 
+// let val5 = ' ' && 'ruchi'
+// console.log(val4)    //undefined
+
+// let val6 = 3 && 'ruchi'
+// console.log(val6)    //undefined
+
+
+//-------------------------program2
+//|| -->1 st value true hogye to short circuit krdenga
+let val1 = 0 || 2<1
+console.log(val1)  //false
+
+let val2 = 3>2 || 2<1
+console.log(val2)   //true
+
+let val3 = " " || 2<1
+console.log(val3)        //space 
+
+let val4 = 'ruchi'|| 2<1
+console.log(val4)        //true
+
+//-------------------------program3
+// null coalescing
+// 0 -----> valid value 
+// let a = 0
+// let a1 =  a || 'ruchi'
+// console.log(a1)
+
+// if(a1>=0){
+//     console.log('hye')
 // }
-// add(2,3)
+//------------------------------------------
 
-// function add(...arr){
-// console.log(arr)
-// return arr.reduce(function(acc,el){
-//     return acc + el
-// },0)
-// }
-// let x = add(10,2)
-// console.log(x)
+//null or undefined ??
+let a  = ' '?? 'rupal'
+console.log(a)
+let a1  = null?? 'rupal'
+console.log(a1)            //rupal
+let a2 = undefined ?? 'rupali'
+console.log(a2)
+let a3  = 'riya '?? 'rupal'
+console.log(a3)
 
-// let x = [1,2,3,4]
+//-------------------------program4
+// optional parameter
 
-// function add(a,b,c,d){
-//     console.log(a)
-//     console.log(b)
-//     console.log(c)
-//     console.log(d)
-// }
-// add(...x)
-
-// let name = ['ruchi','puchi']
-// function na(a,b){
-//     console.log(a)
-//     console.log(b)
-// }
-// na(...name)
-
-// let x = [3,4,6,7,8]
-// function add(a,b,...d){
-//     console.log(a)
-//     console.log(b)
-//     console.log(d)
-// }
-// add(...x)
-
-// let y = [10,20]
-// function add(a,...b){
-//   console.log(a)
-//   console.log(b)
-// }
-// add(...y)
-
-// let a = [2,3,4]
-// let b = [5,6]
-// let c = [...a,...b]
-// console.log(c)
-
-
-// let stud = {
-//     fname : 'ruchi',
-//     lname : 'kumbhare',
-//     age : 22
-// }
-// let{fname,...others} = stud
-// console.log(fname)
-// console.log(others)
-
-// let emp = {
-//     fname : 'sunita',
-//     lname : 'arora',
-//     no : 123
-// }
-// let{fname,...others}= emp
-// console.log(fname)
-// console.log(others)
-
-
-// let A_person = true
-// let B_person = {name : 'vinit'}
-// let val = A_person || B_person
-// console.log(val)
-
-
-let A_person = true
-let B_person = {name: ' nimit'}
-let val1 = 3>2 && 0 
-let val2 = 2 > 3 && 0
-console.log(val1)
-console.log(val2)
-
-
-if(undefined){
-    console.log('hye')
+let emp = {
+    fname : 'raj',
+    lname : 'kapoor',
+    parent:{
+        mname:'sunidhi',
+        lname:'kapoor'
+    },
+    father:'sonu'
 }
-else{
-    console.log('bye')
-}
+// console.log(emp.parent?.mothe) undefined
+//console.log(emp.paren.mname) //Cannot read properties of undefined (reading 'mname')
+console.log(emp.parent?.lname) //kapoor
+
+
+
+
+
+
